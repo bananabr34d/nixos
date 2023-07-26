f82c3b55ff71d10da10ef22d15f49d4e394604dc
Joe <joe@truckstop.cloud>
2023-07-26 18:12:49 -0500

    moved firefox to home manager and applied default settings

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -1,26 +1,27 @@
-{ config, pkgs, ... }:
-
-# Home Manager programs.firefox style
-programs.firefox = {
-  enable = true;
-  package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
-    extraPolicies = {
-      CaptivePortal = false;
-      DisableFirefoxStudies = true;
-      DisablePocket = true;
-      DisableTelemetry = true;
-      DisableFirefoxAccounts = true;
-      FirefoxHome = {
-        Pocket = false;
-        Snippets = false;
+{ pkgs, ... }:
+{
+  # Home Manager programs.firefox style
+  programs.firefox = {
+    enable = true;
+    package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
+      extraPolicies = {
+        CaptivePortal = false;
+        DisableFirefoxStudies = true;
+        DisablePocket = true;
+        DisableTelemetry = true;
+        DisableFirefoxAccounts = true;
+        FirefoxHome = {
+          Pocket = false;
+          Snippets = false;
+        };
+        UserMessaging = {
+          ExtensionRecommendations = false;
+          SkipOnboarding = true;
+        };
       };
-      UserMessaging = {
-        ExtensionRecommendations = false;
-        SkipOnboarding = true;
+      extraPolicies = {
+        ExtensionSettings = {};
       };
-    };
-    extraPolicies = {
-      ExtensionSettings = {};
     };
   };
 }
