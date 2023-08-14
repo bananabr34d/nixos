58deb98d2dafcaf41ed116ceff89aca50e28e539
Joe <joe@truckstop.cloud>
2023-08-14 18:52:45 -0500

    changed fzf recursive searching to catppuccin theme

--- home/home.nix
+++ home/home.nix
@@ -70,33 +70,33 @@
     fzf = {
       enable = true;
       # catppuccin theme
-      # colors = {
-      #   "bg+" = "#313244";
-      #   "bg" = "#1e1e2e";
-      #   "spinner" = "#f5e0dc";
-      #   "hl" = "#f38ba8";
-      #   "fg" = "#cdd6f4";
-      #   "header" = "#f38ba8";
-      #   "info" = "#cba6f7";
-      #   "pointer" = "#f5e0dc";
-      #   "marker" = "#f5e0dc";
-      #   "fg+" = "#cdd6f4";
-      #   "prompt" = "#cba6f7";
-      #   "hl+" = "#f38ba8";
-      # Tokyo Night theme
       colors = {
-        "bg+" = "#000000";
-        "bg" = "#004687";
-        "spinner" = "#00ffd2";
-        "hl" = "#ff4499";
-        "fg" = "#004687";
-        "header" = "#ff4499";
-        "info" = "#e0af68";
-        "pointer" = "#00ffd2";
-        "marker" = "#00ffd2";
-        "fg+" = "#004687";
-        "prompt" = "#e0af68";
-        "hl+" = "#ff4499";
+        "bg+" = "#313244";
+        "bg" = "#1e1e2e";
+        "spinner" = "#f5e0dc";
+        "hl" = "#f38ba8";
+        "fg" = "#cdd6f4";
+        "header" = "#f38ba8";
+        "info" = "#cba6f7";
+        "pointer" = "#f5e0dc";
+        "marker" = "#f5e0dc";
+        "fg+" = "#cdd6f4";
+        "prompt" = "#cba6f7";
+        "hl+" = "#f38ba8";
+      #  Tokyo Night theme
+      # colors = {
+      #   "bg+" = "#000000";
+      #   "bg" = "#004687";
+      #   "spinner" = "#00ffd2";
+      #   "hl" = "#ff4499";
+      #   "fg" = "#004687";
+      #   "header" = "#ff4499";
+      #   "info" = "#e0af68";
+      #   "pointer" = "#00ffd2";
+      #   "marker" = "#00ffd2";
+      #   "fg+" = "#004687";
+      #   "prompt" = "#e0af68";
+      #   "hl+" = "#ff4499";
       };
     };
   };
