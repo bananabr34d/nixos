662d12f19819eb175021571c466e3734d653a106
Joe <joe@truckstop.cloud>
2023-08-14 18:54:53 -0500

    removed btrfs-assistant ref I forgot in default.nix

--- hosts/oxygen/default.nix
+++ hosts/oxygen/default.nix
@@ -103,7 +103,6 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    btrfs-assistant
     curl
     git
     gh
